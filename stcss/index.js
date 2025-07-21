7cbebedcb3a215d5f90f3e7a0039da7e3a78fc6d
GwharshadSalimi <gawharshadsalimi3@gmail.com>
2025-07-21 16:22:26 +0430

    some change in jva

--- stcss/index.js
+++ stcss/index.js
@@ -1,56 +1,28 @@
-function showWather(respones) {
-  let numberInput = document.querySelector("#number");
-  let searchCity = document.querySelector("#city");
-  searchCity.innerHTML = respones.data.city;
-
-  let descreptionElement = document.querySelector("#description");
-  descreptionElement.innerHTML = respones.data.condition.description;
-
-  console.log(respones.data);
-  let humidityElemint = document.querySelector("#humidity");
-  humidityElemint.innerHTML = `${respones.data.temperature.humidity}%`;
-
-  let windElemint = document.querySelector("#wind");
-  windElemint.innerHTML = `${respones.data.wind.speed}km/hr`;
-
-  let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000);
-  console.log(respones);
-  timeElement.innerHTML = formatDate(date);
-
+function refreshWeather(respones) {
+  let numberElement = document.querySelector("#number");
   let temperature = respones.data.temperature.current;
-  numberInput.innerHTML = Math.round(temperature);
+  let h1Element = document.querySelector("#city");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+
+  humidityElement.innerHTML = `${respones.data.temperature.humidity}%`;
+  windElement.innerHTML = `${respones.data.wind.speed}km/hr`;
+  h1Element.innerHTML = respones.data.city;
+  numberElement.innerHTML = Math.round(temperature);
 }
 
-function formatDate(date) {
-  let houres = date.getHoures();
-  let minutes = date.getMinutes();
-  let days = [
-    "saturday",
-    "sunday",
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-  ];
-  let day = days[date.getDay()];
-
-  return `${day} ${houres}:${minutes}`;
+function changTemperature(city) {
+  let apiKey = "19abto433b3602a6dd0fc334c03ec3cf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
-function searchTown(city) {
-  let apikey = "19abto433b3602a6dd0fc334c03ec3cf";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric `;
-  axios.get(apiUrl).then(showWather);
-}
-
-function handelSearch(event) {
+function searchCity(event) {
   event.preventDefault();
-  let searchElement = document.querySelector("#search-input");
+  let searchelement = document.querySelector("#search-input");
 
-  searchTown(searchElement.value);
+  changTemperature(searchelement.value);
 }
 
-let form = document.querySelector(".search-form");
-form.addEventListener("submit", handelSearch);
+let formElement = document.querySelector(".search-form");
+formElement.addEventListener("submit", searchCity);
